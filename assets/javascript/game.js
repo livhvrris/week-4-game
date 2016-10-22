7d35287d3079ca67457ad426f72f4a3b372d53cf
Olivia Harris <livhvrris@Olivias-MacBook-Pro.local>
2016-10-21 20:33:35 -0700

    HTML complete, CSS & JS have started

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+$("#crystalOne").on("click", function(){
+	console.log("!")
+});
+$("#crystalTwo").on("click", function(){
+	console.log("!!")
+});
+$("#crystalThree").on("click", function(){
+	console.log("!!!")
+});
+$("#crystalFour").on("click", function(){
+	console.log("!!!!")
+});
