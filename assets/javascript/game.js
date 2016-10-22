823ce087249af02797896fb241412c8d38785219
Olivia Harris <livhvrris@Olivias-MacBook-Pro.local>
2016-10-21 21:02:26 -0700

    JS halfway complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,28 @@
+var wins = 0;
+var losses = 0;
+
+function crystalsCollectorGame() {
+
+	var minimumNumber = 20;
+    var randomNumber = Math.floor((Math.random() * 100) + 10);
+    
+    $('#randomNumber').html('<h2>' + randomNumber + '</h2>');
+
+	var crystalOne = Math.floor((Math.random() * 25) + 10);
+		console.log(crystalOne);
+	var crystalTwo = Math.floor((Math.random() * 25) + 10);
+		console.log(crystalTwo);
+	var crystalThree = Math.floor((Math.random() * 25) + 10);
+		console.log(crystalThree);
+	var crystalFour = Math.floor((Math.random() * 25) + 10);
+		console.log(crystalFour);
+
+}
+
+crystalsCollectorGame();
+
+
+
 $("#crystalOne").on("click", function(){
 	console.log("!")
 });
