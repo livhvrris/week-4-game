ee60f37d6a45e2d3d6d5a5d893c0e60b549272bd
Olivia Harris <livhvrris@Olivias-MacBook-Pro.local>
2016-10-26 21:26:17 -0700

    JS Bug Fixes in Place

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,54 +3,68 @@ var losses = 0;
 
 var score = 0;
 
+var randomizedNumber = 0;
+
+var crystalOne = 0;
+var crystalTwo = 0;
+var crystalThree = 0;
+var crystalFour = 0;
+
 function valueSetup() {
     
-    var randomNumber = Math.floor((Math.random() * 100) + 10);
-    $('#randomNumber').html('<h2>' + randomNumber + '</h2>');
+    randomizedNumber = Math.floor((Math.random() * 100) + 10);
+    $('#randomNumber').html('<h2>' + randomizedNumber + '</h2>');
 
+	score = 0;
 	$('#score').html('<h2> Your Total: <br></h2> <h1>0</h1>');
+
+    crystalOne = Math.floor((Math.random() * 25));
+	crystalTwo = Math.floor((Math.random() * 25));
+	crystalThree = Math.floor((Math.random() * 25));
+	crystalFour = Math.floor((Math.random() * 25));
 
 }
 
 valueSetup();
 
 function winOrLose() {
-	if (score > randomNumber) {
+	if (score > randomizedNumber) {
 		alert('BUST! You Lose!');
 		losses++;
-		console.log(losses);
+		valueSetup();
+		$('#losses').html('Losses: ' + losses);
 	}
-	else if (score == randomNumber) {
+	else if (score == randomizedNumber) {
 		alert('You Win!');
 		wins++;
-		console.log(wins);
+		valueSetup();
+		$('#wins').html('Wins: ' + wins);
 	}
 }
 
-winOrLose();
+
 
 function crystalCollectorGame() {
-
-	var crystalOne = Math.floor((Math.random() * 25) + 10);
-	var crystalTwo = Math.floor((Math.random() * 25) + 10);
-	var crystalThree = Math.floor((Math.random() * 25) + 10);
-	var crystalFour = Math.floor((Math.random() * 25) + 10);
 
 	$("#crystalOne").on("click", function(){
 		score = score + crystalOne;
 		$('#score').html('<h2> Your Total: <br>' + '<h1>' + score + '</h1>' + '</h2>');
+		winOrLose();
 	});
 	$("#crystalTwo").on("click", function(){
 		score = score + crystalTwo;
 		$('#score').html('<h2> Your Total: <br>' + '<h1>' + score + '</h1>' + '</h2>');
+		winOrLose();
 	});
 	$("#crystalThree").on("click", function(){
 		score = score + crystalThree;
 		$('#score').html('<h2> Your Total: <br>' + '<h1>' + score + '</h1>' + '</h2>');
+		winOrLose();
 	});
 	$("#crystalFour").on("click", function(){
 		score = score + crystalFour;
 		$('#score').html('<h2> Your Total: <br>' + '<h1>' + score + '</h1>' + '</h2>');
+		winOrLose();
 	});
 
 }
